f26d28246a59f5e005b27d776f61e2050caf95d1
Aqeel raza <aqeel.raza@funavry.com-at-890242958078>
2021-12-27 16:26:35 +0500

    chnages db config staging to testing

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require('sequelize');
 
 // testing
-// const sequelize = new Sequelize('eduOpening', 'dbuser', '3e#qv/vdYktS', {
-//   host: '3.209.204.33',
-//   dialect: 'mysql'
-// })
-
-//staging
-const sequelize = new Sequelize('edu_opening', 'dbuser', '3e#qv/vdYktS', {
-  host: '54.227.155.120',
+const sequelize = new Sequelize('eduOpening', 'dbuser', '3e#qv/vdYktS', {
+  host: '3.209.204.33',
   dialect: 'mysql'
 })
+
+//staging
+// const sequelize = new Sequelize('edu_opening', 'dbuser', '3e#qv/vdYktS', {
+//   host: '54.227.155.120',
+//   dialect: 'mysql'
+// })
 
 
 // const sequelize = new Sequelize('edutest','root','root',{
