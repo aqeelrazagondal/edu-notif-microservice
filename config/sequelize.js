7f6608f05c3985eb952f56d414c6ac327211d385
Aqeel raza <aqeel.raza@funavry.com-at-890242958078>
2021-12-30 14:26:07 +0500

    added Prod credentials

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 // testing
-const sequelize = new Sequelize('eduOpening', 'dbuser', '3e#qv/vdYktS', {
-  host: '3.209.204.33',
-  dialect: 'mysql'
-})
+// const sequelize = new Sequelize('eduOpening', 'dbuser', '3e#qv/vdYktS', {
+//   host: '3.209.204.33',
+//   dialect: 'mysql'
+// })
 
 //staging
 // const sequelize = new Sequelize('edu_opening', 'dbuser', '3e#qv/vdYktS', {
@@ -17,6 +17,11 @@ const sequelize = new Sequelize('eduOpening', 'dbuser', '3e#qv/vdYktS', {
 //   host:'localhost',
 //   dialect:'mysql'
 // })
+
+const sequelize = new Sequelize('eduOpeningProduction', 'eduopening', 'T1G&D#b%D3V', {
+  host: 'eduopening.cam478cwlohp.us-east-1.rds.amazonaws.com',
+  dialect: 'mysql'
+})
 
 
 const p = sequelize.authenticate();
